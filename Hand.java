40ebd600168f4aaa1bde3af34783b664de67ccfb
taiseth <taiseth@utexas.edu>
2016-04-20 13:54:20 -0500

    finished hand class for now

--- Hand.java
+++ Hand.java
@@ -20,8 +20,48 @@ public class Hand {
 		this.size++;
 	}
 	
+	/*
+	 * Returns the card removed from the hand
+	 * given the index 
+	 */
 	public Card give(int index)
 	{
 		return hand.remove(index);
+	}
+	
+	/*
+	 * Returns the card removed from the hand
+	 * given a value and suit
+	 * Returns null if the card does not exist in the hand 
+	 */
+	public Card give(int value, String suit)
+	{
+		Card c = new Card(value, suit);
+		int index = hand.indexOf(c); //finds where card is located in hand
+		if(index == -1)
+		{
+			return null;
+		}
+		else
+		{
+			return hand.remove(index);
+		}
+	}
+	
+	/*
+	 * Makes a copy of the hand before clearing it
+	 * Returns the copy of the hand before it was cleared
+	 */
+	public ArrayList<Card> empty()
+	{
+		ArrayList<Card> cards = new ArrayList<Card>();
+		for(int i = 0 ; i < hand.size(); i++)
+		{
+			Card c = new Card((hand.get(i)).getValue(), (hand.get(i)).getSuit());
+			cards.add(c);
+		}
+		
+		hand.clear();
+		return cards;		
 	}
 }
